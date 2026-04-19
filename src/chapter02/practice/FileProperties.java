1e86daeb88c05d4a7e4dd246aecf9074b6e5c190
yoko345 <spblackhole89@gmail.com>
2026-04-19 16:34:08 +0900

    練習問題（模範解答）〜問題をよく読んでいなかった。java.util.Propertiesクラスを使用してコーディングする必要があったのに、無視した実装にしてしまった。〜

--- src/chapter02/practice/FileProperties.java
+++ src/chapter02/practice/FileProperties.java
@@ -4,39 +4,33 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Paths;
+import java.util.Properties;
 
-public class FileProperties extends Properties implements FileIO {
+public class FileProperties implements FileIO {
+    Properties property = new Properties();
+
     private String path;
     private StringBuilder basePath;
-    private String comments = "";
 
     @Override
     public String getValue(String key) {
-        return null;
+        return property.getProperty(key);
     }
 
     @Override
     public void readFromFile(String fileName) throws IOException {
-        try (FileReader reader = new FileReader(getBasePath().append(fileName).toString())) {
-            load(reader);
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        property.load(new FileReader(getBasePath().append(fileName).toString()));
     }
 
     @Override
     public void setValue(String key, String value) {
-        comments += key + "=" + value + "\n";
+        property.setProperty(key, value);
     }
 
     @Override
     public void writeToFile(String fileName) throws IOException {
-        try (FileWriter writer = new FileWriter(getBasePath().append(fileName).toString())) {
-            store(writer, comments);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        property.store(new FileWriter(getBasePath().append(fileName).toString()),
+                "written by FileProperties");
     }
 
     private StringBuilder getBasePath() {
