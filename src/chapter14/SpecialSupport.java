e86090035e55e73e2d9a387a69874ecc5133b908
yoko345 <spblackhole89@gmail.com>
2026-08-23 10:49:32 +0900

    Chain of Responsibilityの学習

---
+++ src/chapter14/SpecialSupport.java
@@ -0,0 +1,15 @@
+package chapter14;
+
+public class SpecialSupport extends Support {
+    private int specialNumber;
+
+    public SpecialSupport(String name, int specialNumber) {
+        super(name);
+        this.specialNumber = specialNumber;
+    }
+
+    @Override
+    protected boolean resolve(Trouble trouble) {
+        return trouble.getTroubleNumber() == specialNumber;
+    }
+}
---
+++ src/chapter14/Support.java
@@ -0,0 +1,46 @@
+package chapter14;
+
+public abstract class Support {
+    private String name;
+    private Support nextSupport;
+
+    public Support(String name) {
+        this.name = name;
+        this.nextSupport = null;
+    }
+
+    // たらい回し先の設定
+    public Support setNextSupport(Support nextSupport) {
+        this.nextSupport = nextSupport;
+        return nextSupport;
+    }
+
+    // トラブル解決の手順を定める
+    public void support(Trouble trouble) {
+        if (resolve(trouble)) {
+            done(trouble);
+        } else if (nextSupport != null) {
+            nextSupport.support(trouble);
+        } else {
+            fail(trouble);
+        }
+    }
+
+    // 解決しようとする
+    protected abstract boolean resolve(Trouble trouble);
+
+    // 解決した
+    public void done(Trouble trouble) {
+        System.out.println(trouble + " is resolved by " + this + ".");
+    }
+
+    // 解決しなかった
+    public void fail(Trouble trouble) {
+        System.out.println(trouble + " cannot be resolved.");
+    }
+
+    @Override
+    public String toString() {
+        return "[" + name + "]";
+    }
+}
