2e4efc907d12ab87aae883b7c78e7d357e38c6a9
yoko345 <spblackhole89@gmail.com>
2026-08-30 15:09:52 +0900

    テキストブロックの学習

--- src/chapter15/MainChapter15.java
+++ src/chapter15/MainChapter15.java
@@ -9,5 +9,35 @@ public class MainChapter15 {
 
         // 練習問題15-2
         PageMaker.makeLinkPage("linkpage.html");
+
+        // 練習問題15-3
+        // テキストブロックを使用した学習
+        String html = """
+                <!DOCTYPE html>
+                <html>
+                    <head>
+                        <title>Welcome!</title>
+                    </head>
+                    <body>
+                        <h1 style="text-align: center">Hello World!</h1>
+                    </body>
+                </html>
+                """;
+        System.out.println(html);
+
+        String title = "Welcome!!!";
+        String message = "Hello World!!!";
+        String html1 = """
+                <!DOCTYPE html>
+                <html>
+                    <head>
+                        <title>%s</title>
+                    </head>
+                    <body>
+                        <h1 style="text-align: center">%s</h1>
+                    </body>
+                </html>
+                """.formatted(title, message);
+        System.out.println(html1);
     }
 }
