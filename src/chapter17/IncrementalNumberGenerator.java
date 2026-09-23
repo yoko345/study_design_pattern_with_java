5a42ab0b6ba060eb0cd8a74d97b07857469f995f
yoko345 <spblackhole89@gmail.com>
2026-09-24 07:05:11 +0900

    練習問題17-1（模範解答）

--- src/chapter17/IncrementalNumberGenerator.java
+++ src/chapter17/IncrementalNumberGenerator.java
@@ -2,12 +2,14 @@ package chapter17;
 
 public class IncrementalNumberGenerator extends NumberGenerator {
     private int number;
-    private int startNumber;
+    // private int startNumber;
     private int endNumber;
     private int incrementNumber;
 
-    public IncrementalNumberGenerator(int startNumber, int endNumber, int incrementNumber) {
-        this.startNumber = startNumber;
+    // public IncrementalNumberGenerator(int startNumber, int endNumber, int incrementNumber) {
+    public IncrementalNumberGenerator(int number, int endNumber, int incrementNumber) {
+        // this.startNumber = startNumber;
+        this.number = number;
         this.endNumber = endNumber;
         this.incrementNumber = incrementNumber;
     }
@@ -19,10 +21,14 @@ public class IncrementalNumberGenerator extends NumberGenerator {
 
     @Override
     public void execute() {
-        for (int i = startNumber; i < endNumber;) {
-            number = i;
-            i += incrementNumber;
+        // for (int i = startNumber; i < endNumber;) {
+        //     number = i;
+        //     i += incrementNumber;
+        //     notifyObservers();
+        // }
+        while (number < endNumber) {
             notifyObservers();
+            number += incrementNumber;
         }
     }
 }
