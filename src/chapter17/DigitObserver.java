c515cd6a348213fef28d7f913f947f771ca7d276
yoko345 <spblackhole89@gmail.com>
2026-09-24 07:00:40 +0900

    コード整形

--- src/chapter17/DigitObserver.java
+++ src/chapter17/DigitObserver.java
@@ -4,6 +4,7 @@ public class DigitObserver implements Observer {
     @Override
     public void update(NumberGenerator generator) {
         System.out.println("DigitObservers: " + generator.getNumber());
+
         try {
             Thread.sleep(100);
         } catch (InterruptedException e) {
